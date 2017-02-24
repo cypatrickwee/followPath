4f30cf93d48775774ec196cc0c0063ef2be87e9a
Patrick KWEE Chun Yoong <cy.patrickwee@gmail.com>
2017-02-24 16:12:04 +0800

    Unity sample path follow system by using waypoint.
    Unity sample path follow system by using waypoint.

---
+++ Assets/Scripts/pathAction.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pathAction : MonoBehaviour {
+
+    //let the object to know which path that has the pathEngine script within
+    public pathEngine pathToFollow;
+    public int currentWayPointID = 0;//int array of element in the list
+    public float speed;
+    public float rotationSpeed = 1.0f;
+    public string pathName;
+
+    Vector3 last_position;
+    Vector3 current_position;
+
+    private int indexes;
+    private float reachDistance = 0.0f; //distance between the object origin and the point that is tangent to it.
+    // Use this for initialization
+	void Start () {
+        //pathToFollow = GameObject.Find(pathName).GetComponent<pathEngine>(); //used only during object instantiation, notice we can select the path
+        last_position = transform.position;
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //take a look at below syntax, pathToFollow.path_objs[index].position
+        //pathToFollow has access the previous list array in script pathEngine.
+        float distance = Vector3.Distance(pathToFollow.path_objs[currentWayPointID].position, transform.position);// return the distance between a and b, 
+        //the last arguement in the below syntax is a time variable
+        transform.position = Vector3.MoveTowards(transform.position, pathToFollow.path_objs[currentWayPointID].position, Time.deltaTime * speed);
+
+        if(distance <= reachDistance)
+        {
+            currentWayPointID++;
+        }
+        //below code is to reset back to original position (notice that its not Count-1 as currentWayPointID has already ++)
+        if(currentWayPointID == pathToFollow.path_objs.Count)
+        {
+            currentWayPointID = 0;
+        }
+
+    }
+}
---
+++ Assets/Scripts/pathEngine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class pathEngine : MonoBehaviour
+{
+
+    public Color rayColor = Color.cyan;
+    public List<Transform> path_objs = new List<Transform>();//declare a list
+    Transform[] theArray;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor; //the cyan color line
+        theArray = GetComponentsInChildren<Transform>();
+        path_objs.Clear();
+
+        foreach (Transform path_obj in theArray)
+        {
+            if (path_obj != this.transform)
+            {
+                path_objs.Add(path_obj);
+            }
+        }
+
+        for (int i = 0; i < path_objs.Count; i++)
+        {
+            Vector3 position = path_objs[i].position;
+            if (i > 0)
+            {
+                Vector3 previous = path_objs[i - 1].position;
+                Gizmos.DrawLine(previous, position);
+                Gizmos.DrawWireSphere(position, 0.3f); //draw the sphere in the current position
+            }
+
+
+        }
+    }
+}
